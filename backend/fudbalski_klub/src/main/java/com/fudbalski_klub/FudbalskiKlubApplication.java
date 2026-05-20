67d7bca8b60d7a072814a4ae39f477e42c4bda30
Nikola Vulović <nikolavulovic4@gmail.com>
2026-05-20 17:09:12 +0200

    initial commit

---
+++ backend/fudbalski_klub/src/main/java/com/fudbalski_klub/FudbalskiKlubApplication.java
@@ -0,0 +1,13 @@
+package com.fudbalski_klub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudbalskiKlubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudbalskiKlubApplication.class, args);
+	}
+
+}
---
+++ backend/fudbalski_klub/src/test/java/com/fudbalski_klub/FudbalskiKlubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudbalski_klub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudbalskiKlubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
