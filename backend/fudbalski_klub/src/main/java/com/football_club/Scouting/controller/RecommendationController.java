7ee33a17214ad5f383317f5a66a9f755b9b95496
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-06-18 01:04:00 +0200

    feat: Forgot to actually add RecommendationController.

---
+++ backend/fudbalski_klub/src/main/java/com/football_club/Scouting/controller/RecommendationController.java
@@ -0,0 +1,38 @@
+package com.football_club.Scouting.controller;
+
+import com.football_club.Scouting.dto.PlayerRecommendationDTO;
+import com.football_club.Scouting.dto.RecommendationRequestDTO;
+import com.football_club.Scouting.service.IRecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/recommendations")
+@RequiredArgsConstructor
+public class RecommendationController {
+    private final IRecommendationService recommendationService;
+
+    public ResponseEntity<List<PlayerRecommendationDTO>> getRecommendations(
+            @RequestBody RecommendationRequestDTO request
+            ) {
+        if (request == null || request.getPosition() == null || request.getMetricWeights() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<PlayerRecommendationDTO> result = recommendationService.getRecommendations(
+                request.getPosition(),
+                request.getMetricWeights()
+                        .stream()
+                        .collect(Collectors
+                                .toMap(RecommendationRequestDTO.WeightedMetrics::getMetricdId,
+                                        RecommendationRequestDTO.WeightedMetrics::getWeight))
+        );
+        return ResponseEntity.ok(result);
+    }
+}
