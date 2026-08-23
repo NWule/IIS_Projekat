656ae884a79475781e4c0a286e40c811ae128fd9
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-08-23 23:30:13 +0200

    feat: Added automatic generation of DTO classes for API Football based on JSON response samples. Added one test request.

--- backend/fudbalski_klub/src/main/java/com/football_club/Clients/APIFootballClient.java
+++ backend/fudbalski_klub/src/main/java/com/football_club/Clients/APIFootballClient.java
@@ -1,7 +1,7 @@
 package com.football_club.Clients;
 
+import com.football_club.dto.apifootball.TeamSearch;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -9,4 +9,12 @@ import org.springframework.web.client.RestClient;
 @RequiredArgsConstructor
 public class APIFootballClient {
     private final RestClient restClient;
+
+    // Search for a team by name
+    public TeamSearch searchTeams(String teamName) {
+        return restClient.get()
+                .uri("/teams?search={name}", teamName)
+                .retrieve()
+                .body(TeamSearch.class);
+    }
 }
